d2b1664f9bac523a86f8688532c788ede26c1a8e
Om Patel <ompatel34003@gmail.com>
2025-10-24 09:27:18 +0530

    added frontend auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,34 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+
 const App = () => {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
+    <>
       <SignedIn>
-        <UserButton />
+        {/* <UserButton /> */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </Routes>
+        <Routes>
+          <Route path="/auth" element={<HomePage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </Routes>
       </SignedIn>
-    </header>
+
+      <SignedOut>
+        {/* <SignInButton mode="modal" /> */}
+        <Routes>
+          {" "}
+          <Route path="/auth" element={<AuthPage />} />{" "}
+          <Route path="*" element={<Navigate to="/auth" replace />} />{" "}
+        </Routes>
+      </SignedOut>
+    </>
   );
 };
 
